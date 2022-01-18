34303e9c9e336375702863988e8c93ec38447c97
AlberLC <alberlc@outlook.com>
2022-01-18 02:27:27 +0100

    Access mongodb with user and pass

--- multibot/models/database.py
+++ multibot/models/database.py
@@ -1,4 +1,7 @@
+import os
+
 import pymongo
 
-mongo_client = pymongo.MongoClient("localhost", 27017)
+mongo_client = pymongo.MongoClient("localhost", 27017, username=os.environ.get('MONGO_USER'), password=os.environ.get('MONGO_PASSWORD'))
+
 db = mongo_client.flanabot
