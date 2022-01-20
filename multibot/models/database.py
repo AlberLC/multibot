1844cf99c056281f4015a8abee1c8f0dd7e4a3da
AlberLC <alberlc@outlook.com>
2022-01-20 02:48:31 +0100

    Access mongodb with host and port

--- multibot/models/database.py
+++ multibot/models/database.py
@@ -2,6 +2,11 @@ import os
 
 import pymongo
 
-mongo_client = pymongo.MongoClient("localhost", 27017, username=os.environ.get('MONGO_USER'), password=os.environ.get('MONGO_PASSWORD'))
+mongo_client = pymongo.MongoClient(
+    host=os.environ.get('MONGO_HOST'),
+    port=int(port) if (port := os.environ.get('MONGO_PORT')) else None,
+    username=os.environ.get('MONGO_USER'),
+    password=os.environ.get('MONGO_PASSWORD')
+)
 
 db = mongo_client.flanabot
