aca3b9dd30a83b962996fd2701734c5e504b488f
AlberLC <alberlc@outlook.com>
2022-10-15 05:36:58 +0200

    Update exception handler message

--- multibot/bots/multi_bot.py
+++ multibot/bots/multi_bot.py
@@ -309,8 +309,8 @@ class MultiBot(Generic[T], ABC):
         try:
             await callback(message)
         except Exception:
-            if constants.SEND_EXCEPTION_MESSAGE:
-                traceback_message = '\n'.join(traceback.format_exc().splitlines()[-10:])
+            if constants.SEND_EXCEPTION_MESSAGE_LINES:
+                traceback_message = '\n'.join(traceback.format_exc().splitlines()[-constants.SEND_EXCEPTION_MESSAGE_LINES:])
                 await self.send(f'{random.choice(constants.EXCEPTION_PHRASES)}\n'
                                 f'\n'
                                 f'...\n'
