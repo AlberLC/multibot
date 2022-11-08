0f4f0d6b07c773dc05011418feab57092fbafd39
AlberLC <alberlc@outlook.com>
2022-11-08 03:23:02 +0100

    Move logic from FlanaBot

--- multibot/bots/multi_bot.py
+++ multibot/bots/multi_bot.py
@@ -19,7 +19,7 @@ import functools
 import random
 import traceback
 from abc import ABC, abstractmethod
-from typing import Any, Callable, Generic, Iterable, Type, TypeVar, overload
+from typing import Any, Callable, Generic, Iterable, Sequence, Type, TypeVar, overload
 
 import discord
 import flanautils
@@ -295,6 +295,29 @@ class MultiBot(Generic[T], ABC):
                     await self._remove_penalty(penalty, unpenalize_method)
                 except UserDisconnectedError:
                     pass
+
+    def _distribute_buttons(self, texts: Sequence[str]) -> list[list[str]]:
+        pass
+
+    async def _filter_mention_ids(self, text: str | Iterable[str], message: Message, delete_names=False) -> list[str]:
+        if isinstance(text, str):
+            words = text.split()
+        else:
+            words = text
+
+        ids = []
+        if delete_names:
+            for user in message.mentions:
+                ids.append(user.name.lower())
+                ids.append(user.name.split('#')[0].lower())
+                ids.append(str(user.id))
+        else:
+            for user in message.mentions:
+                ids.append(str(user.id))
+        for role in await self.get_group_roles(message):
+            ids.append(str(role.id))
+
+        return [word for word in words if flanautils.remove_symbols(word).strip() not in ids]
 
     async def _find_users_to_punish(self, message: Message) -> OrderedSet[User]:
         bot_user = await self.get_me(message.chat.group_id)
