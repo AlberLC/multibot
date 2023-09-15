f33daaf28fd977704b59f75ea17081bf2f03b525
AlberLC <alberlc@outlook.com>
2023-09-15 23:11:51 +0200

    Add received messages to cache

--- multibot/bots/multi_bot.py
+++ multibot/bots/multi_bot.py
@@ -408,6 +408,7 @@ class MultiBot(Generic[T], ABC):
             message.resolve()
             message.update_edit_date(await self._get_edit_date(original_message))
             message.save(pull_overwrite_fields=pull_overwrite_fields, pull_lazy=False)
+            self._message_cache[message_id, chat.id] = message
             return message
 
         if cached_message.buttons_info:
